017d967e10ca61f92181db3be6902add958381c9
Pedro Cortés <p.cortes.nieves@gmail.com>
2025-05-22 21:35:55 +0200

    FirstConsoleApp project

---
+++ 01_basics_cpp/FirstConsoleApp/FirstConsoleApp.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    const int MIN_AGE = 18;
+    int age;
+
+    cout << "Please enter your age:" << endl;
+    cin >> age;
+
+    if (age < MIN_AGE) 
+    {
+        cout << "You are not allowed to get into the place." << endl;
+    }
+    else
+    {
+        cout << "Perfect, please follow the white rabit.";
+    }
+
+    cout << "End of the program.";
+}
