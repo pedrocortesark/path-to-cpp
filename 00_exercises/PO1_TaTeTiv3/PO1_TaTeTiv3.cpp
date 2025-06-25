a9a4f3973a36cb240d4b56624a3c8006e44457e3
pedrocortes <p.cortes.nieves@gmail.com>
2025-06-25 12:06:22 +0200

    Modify TaTeTi project with struts and functions

--- 00_exercises/PO1_TaTeTiv3/PO1_TaTeTiv3.cpp
+++ 00_exercises/PO1_TaTeTiv3/PO1_TaTeTiv3.cpp
@@ -10,24 +10,70 @@ const int NUM_FILAS = 3;
 const int NUM_COLUMNAS = 3;
 
 /**
+    @enum       EstadoJuego
+    @brief      El estado del juego en un momento
+                dado puede ser INICIADO, FINALIZADO (cuando
+                un jugador ganó) o EMPATE (cuando nadie ganó).
+**/
+enum EstadoJuego { INICIADO, FINALIZADO, EMPATE };
+
+/**
+    @struct     TaTeTi
+    @brief      Representa el juego del TATETI.
+                Contiene el tablero de juego, el turno
+                actual y el estado del juego.
+**/
+struct TaTeTi {
+    char tablero[NUM_FILAS][NUM_COLUMNAS];
+    char turno;
+    EstadoJuego estado;
+};
+
+/**
     @brief      Retorna JUGADOR_O o JUGADOR_X aleatoriamente con un 50% de probabilidad.
     @retval     JUGADOR_O o JUGADOR_X, uno de ambos valores al azar.
 **/
 char jugadorInicial()
 {
+    srand(time(NULL));
     if (rand() % 2 == 0) return JUGADOR_X;
     else return JUGADOR_O;
 }
 
 /**
-@brief Asigna a todas las celdas del tablero el valor CELDA_VACIA.
-@param tablero El tablero a ser inicializado.
+     @brief     Indica cuál es el jugador actual retornando JUGADOR_O o JUGADOR_X.
+     @param t   El juego a consultar.
+     @retval    El carácter JUGADOR_O o JUGADOR_X representando al jugador actual.
 **/
-void inicializarTablero(char tablero[NUM_FILAS][NUM_COLUMNAS])
+char jugadorActual(const TaTeTi& t)
+{
+	return t.turno;
+}
+
+/**
+     @brief     Indica el estado actual del juego.
+     @param t   El juego cuyo estado se quiere consultar.
+     @retval    El estado del juego.
+**/
+EstadoJuego estadoActual(const TaTeTi& t)
+{
+	return t.estado;
+}
+
+/**
+     @brief     Inicializa el juego estableciendo todas las celdas del tablero como
+                CELDA_VACIA. Además se determina al azar el turno actual JUGADOR_O o
+                JUGADOR_X, y se establece el estado como INICIADO.
+     @param t   El juego a inicializar.
+**/
+void inicializarTablero(TaTeTi &t)
 {
     for (int i = 0; i < NUM_FILAS; i++)
         for (int j = 0; j < NUM_COLUMNAS; j++)
-            tablero[i][j] = CELDA_VACIA;
+            t.tablero[i][j] = CELDA_VACIA;
+
+	t.turno = jugadorInicial();
+    t.estado = INICIADO;
 }
 
 /**
@@ -41,14 +87,38 @@ void inicializarTablero(char tablero[NUM_FILAS][NUM_COLUMNAS])
                 JUGADOR_O y JUGADOR_X para las posiciones donde cada jugador ha marcado.
      @param     tablero - Un arreglo bidimensional de caracters de 3x3 que representa al tablero de juego.
 **/
-void imprimirTablero(char tablero[3][3]) 
+//void imprimirTablero(char tablero[3][3])
+void imprimirTablero(TaTeTi t) 
 {
     cout << "\n";
     cout << "\t\t" << "  " << "1" << " " << "2" << " " << "3" << endl;
-    cout << "\t\t" << "1 " << tablero[0][0] << " " << tablero[0][1] << " " << tablero[0][2] << endl;
-    cout << "\t\t" << "2 " << tablero[1][0] << " " << tablero[1][1] << " " << tablero[1][2] << endl;
-    cout << "\t\t" << "3 " << tablero[2][0] << " " << tablero[2][1] << " " << tablero[2][2] << endl;
+    cout << "\t\t" << "1 " << t.tablero[0][0] << " " << t.tablero[0][1] << " " << t.tablero[0][2] << endl;
+    cout << "\t\t" << "2 " << t.tablero[1][0] << " " << t.tablero[1][1] << " " << t.tablero[1][2] << endl;
+    cout << "\t\t" << "3 " << t.tablero[2][0] << " " << t.tablero[2][1] << " " << t.tablero[2][2] << endl;
     cout << "\n";
+}
+
+/**
+     @brief         Indica si las coordenadas dadas son válidas, es decir, que fila y
+                    columna sean valores entre 0 y 2 inclusive, y que además correspondan a
+                    una celda vacía.
+     @param fila    La fila a verificar.
+     @param columna La columna a verificar.
+     @param t       El juego en el que se realiza la verificación.
+     @retval        TRUE si fila y columna están entre 0 y 2 y además la celda a la
+                    que hacen referencia es vacía, FALSE en cualquier otro caso.
+**/
+bool esCeldaValida(int fila, int columna, const TaTeTi& t)
+{
+    if (fila < 0 || fila > NUM_FILAS - 1 || columna < 0 || columna > NUM_COLUMNAS - 1) {
+        cout << "Jugada invalida. Por favor, ingresa una jugada valida (ejemplo: 11, 12, 32)." << endl;
+        return false;
+    }
+    if (t.tablero[fila][columna] != CELDA_VACIA) {
+		cout << "La celda ya esta ocupada. Por favor, ingresa una jugada valida." << endl;
+        return false;
+    }
+	return true;
 }
 
 /**
@@ -56,20 +126,21 @@ void imprimirTablero(char tablero[3][3])
      @param     tablero El tablero de juego.
      @retval    TRUE si hay una fila, columna o diagonal alineada, FALSE si no.
 **/
-bool verificarAlineacion(char tablero[NUM_FILAS][NUM_COLUMNAS])
+//bool verificarAlineacion(char tablero[NUM_FILAS][NUM_COLUMNAS])
+bool verificarAlineacion(const TaTeTi &t)
 {
     for (int i = 0; i < NUM_FILAS; i++)
-        if (tablero[i][0] == tablero[i][1] && tablero[i][1] == tablero[i][2] && tablero[i][0] != CELDA_VACIA)
+        if (t.tablero[i][0] == t.tablero[i][1] && t.tablero[i][1] == t.tablero[i][2] && t.tablero[i][0] != CELDA_VACIA)
             return true;
 
     for (int i = 0; i < NUM_COLUMNAS; i++)
-        if (tablero[0][i] == tablero[1][i] && tablero[1][i] == tablero[2][i] && tablero[0][i] != CELDA_VACIA)
+        if (t.tablero[0][i] == t.tablero[1][i] && t.tablero[1][i] == t.tablero[2][i] && t.tablero[0][i] != CELDA_VACIA)
             return true;
 
-    if (tablero[0][0] == tablero[1][1] && tablero[1][1] == tablero[2][2] && tablero[1][1] != CELDA_VACIA)
+    if (t.tablero[0][0] == t.tablero[1][1] && t.tablero[1][1] == t.tablero[2][2] && t.tablero[1][1] != CELDA_VACIA)
         return true;
 
-    if (tablero[0][2] == tablero[1][1] && tablero[1][1] == tablero[2][0] && tablero[1][1] != CELDA_VACIA)
+    if (t.tablero[0][2] == t.tablero[1][1] && t.tablero[1][1] == t.tablero[2][0] && t.tablero[1][1] != CELDA_VACIA)
         return true;
 
 	return false;
@@ -80,11 +151,12 @@ bool verificarAlineacion(char tablero[NUM_FILAS][NUM_COLUMNAS])
     @param      tablero El tablero de juego.
     @retval     TRUE si hay al menos una celda vacía, FALSE si no.
 **/
-bool hayMovimientosPosibles(char tablero[NUM_FILAS][NUM_COLUMNAS])
+//bool hayMovimientosPosibles(char tablero[NUM_FILAS][NUM_COLUMNAS])
+bool hayMovimientosPosibles(const TaTeTi &t)
 {
     for (int i = 0; i < NUM_FILAS; i++)
         for (int j = 0; j < NUM_COLUMNAS; j++)
-            if (tablero[i][j] == CELDA_VACIA)
+            if (t.tablero[i][j] == CELDA_VACIA)
 				return true;
 	return false;
 }
@@ -104,86 +176,77 @@ bool hayMovimientosPosibles(char tablero[NUM_FILAS][NUM_COLUMNAS])
                 XY donde X e Y son dígitos enteros entre 0 y 9.
 
 **/
-bool leerCoordenadas(int& fila, int& columna)
+bool leerCoordenadas(int& fila, int& columna, const TaTeTi &t)
 {
     string jugada;
-
     cin >> jugada;
-    fila = jugada[0] - '0';
-    columna = jugada[1] - '0';
 
-    if (fila < 1 || fila > 3 || columna < 1 || columna > 3)
+    for(char digit : jugada) {
+        if (!isdigit(digit)) {
+            cout << "Entrada invalida. Por favor, ingresa una jugada valida (ejemplo: 11, 12, 32)." << endl;
+            return false;
+        }
+	}
+
+	fila = stoi(jugada.substr(0, 1)) - 1;
+    columna = stoi(jugada.substr(1, 1)) - 1;
+	return true;
+}
+
+/**
+     @brief         Realiza el movimiento del jugador actual en la celda dada por las
+                    coordenadas [fila][columna]. Si luego del movimiento el jugador logró
+                    una alineación se establecerá el juego como FINALIZADO; si no, en caso
+                    de que no queden movimientos posibles se establecerá como EMPATE. En
+                    cualquier otro caso se intercambiará el jugador y se continúa la partida.
+                    PRECONDICIÓNES - Se asume que:
+                            * esCeldaValida(fila,columna,t)
+                            * hayMovimientosPosibles(t)
+     @param fila    La fila de la celda en la que se actuará.
+     @param columna La columna de la celda en la que se actuará.
+     @param t       El juego en el que se modificará el tablero.
+**/
+void realizarMovimiento(int fila, int columna, TaTeTi& t)
+{
+	t.tablero[fila][columna] = t.turno;
+
+    if(verificarAlineacion(t)) 
     {
-        cout << "Jugada invalida. Por favor, ingresa una jugada valida (ejemplo: 11, 12, 32)." << endl;
-		return false;
+        t.estado = FINALIZADO;
+        imprimirTablero(t);
+        cout << "FIN DE LA PARTIDA. El ganador es: " << t.turno << endl;
+        return;
+    } 
+    else if (!hayMovimientosPosibles(t))
+    {
+        t.estado = EMPATE;
+        cout << "FIN DE LA PARTIDA. Nadie ha ganado." << endl;
+        return;
     }
     else
     {
-        return true;
+        t.turno = (t.turno == JUGADOR_X) ? JUGADOR_O : JUGADOR_X;
     }
 }
 
-
 int main()
 {
-    char tablero[NUM_FILAS][NUM_COLUMNAS];
-    int contador = 0;
-    bool ganador = false;
-    char contenido;
+	TaTeTi juego;
     int fila = 0;
     int columna = 0;
-    char opcion;
 
-    srand(time(NULL));
-
-    // Inicializar la tabla
-	inicializarTablero(tablero);
-
-    // Turno aleatorio
-	opcion = jugadorInicial();
-
-    //Inicio del juego
+	inicializarTablero(juego);
     cout << ">> Bienvenido al juego del Ta Te Ti!" << endl;
 
-    while (!ganador)
+    while (estadoActual(juego) == INICIADO)
     {
-        cout << "Turno del jugador: " << opcion << endl;
-		imprimirTablero(tablero);
+        cout << "Turno del jugador: " << juego.turno << endl;
+		imprimirTablero(juego);
 
-        // Check entrada de jugador y si la celda está vacía
-        do
-        {
-            contenido = (char)0;
+        do{
+            cout << ">> Por favor, ingresa tu siguiente jugada: ";
+        } while (!leerCoordenadas(fila, columna, juego) || !esCeldaValida(fila, columna, juego));
 
-            while (!leerCoordenadas(fila, columna))
-                cout << ">> Por favor, ingresa tu siguiente jugada: ";
-
-            contenido = tablero[fila - 1][columna - 1];
-
-            if (contenido != CELDA_VACIA)
-                cout << " Ingresa la posicion de una celda vacia." << endl;
-
-        } while (contenido != CELDA_VACIA);
-
-
-        // Finalizamos jugada
-        tablero[fila - 1][columna - 1] = opcion;
-
-        if (verificarAlineacion(tablero))
-        {
-			imprimirTablero(tablero);
-            cout << "FIN DE LA PARTIDA. El ganador es: " << opcion << endl;
-            return 0;
-        }
-
-        if (!hayMovimientosPosibles(tablero))
-        {
-            cout << "FIN DE LA PARTIDA. Nadie ha ganado." << endl;
-            return 0;
-        }
-
-        // Cambiamos de jugador
-        if (opcion == JUGADOR_O) opcion = JUGADOR_X;
-        else opcion = JUGADOR_O;
+		realizarMovimiento(fila, columna, juego);
     }
 }
